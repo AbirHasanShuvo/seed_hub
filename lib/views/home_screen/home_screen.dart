b59a0d4ca46ed301da6948442b3f2cb574beac7c
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2024-05-10 09:37:35 +0600

    changed home screen

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,190 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:seed_hub/common_widgets/home_button.dart';
-import 'package:seed_hub/common_widgets/string.dart';
-import 'package:seed_hub/common_widgets/text_widget.dart';
-import 'package:seed_hub/const/const.dart';
-import 'package:seed_hub/const/images.dart';
-import 'package:seed_hub/const/list.dart';
-import 'package:seed_hub/controllers/auth_controller.dart';
-import 'package:velocity_x/velocity_x.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    //hasan@gmail.com
-    var controller = Get.put(AuthController());
     return Scaffold(
-        backgroundColor: lightGrey,
-        body: Container(
-          padding: const EdgeInsets.all(12),
-          width: screenWidth(context),
-          height: screenHeight(context),
-          child: SafeArea(
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 60,
-                  // color: lightGrey,
-                  child: TextFormField(
-                    style: TextStyle(fontFamily: mainFont),
-                    // controller: controller.searchController,
-                    decoration: InputDecoration(
-                        suffixIcon: const Icon(Icons.search).onTap(() {}),
-                        filled: true,
-                        fillColor: whiteColor,
-                        hintText: 'Search',
-                        hintStyle: TextStyle(
-                            color: Colors.grey, fontFamily: mainFont)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            // Other widgets here
+            GridView.count(
+              shrinkWrap: true,
+              // Important
+              physics: NeverScrollableScrollPhysics(),
+              // Disable GridView's scrolling
+              crossAxisCount: 2,
+              children: List.generate(20, (index) {
+                return Center(
+                  child: Text(
+                    'Item $index',
+                    style: TextStyle(fontSize: 24),
                   ),
-                ),
-                10.heightBox,
-                Expanded(
-                  child: SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    child: Column(
-                      children: [
-                        VxSwiper.builder(
-                            height: 150,
-                            autoPlay: true,
-                            aspectRatio: 16 / 9,
-                            enlargeCenterPage: true,
-                            itemCount: firstSlider.length,
-                            itemBuilder: (context, index) => Image.asset(
-                                  fit: BoxFit.fill,
-                                  firstSlider[index],
-                                )
-                                    .box
-                                    .rounded
-                                    .clip(Clip.antiAlias)
-                                    .margin(const EdgeInsets.symmetric(
-                                        horizontal: 8))
-                                    .make()),
-                        10.heightBox,
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: List.generate(
-                              2,
-                              (index) => homeButton(
-                                    width: screenWidth(context) / 2.5,
-                                    height: screenHeight(context) * 0.15,
-                                    icon: index == 0 ? hotDeal : flashSale,
-                                    title:
-                                        index == 0 ? 'Hot deals' : 'Flash sale',
-                                    colors:
-                                        index == 0 ? Colors.green : Colors.blue,
-                                  ).onTap(() {})),
-                        ),
-                        10.heightBox,
-                        VxSwiper.builder(
-                            height: 150,
-                            autoPlay: true,
-                            aspectRatio: 16 / 9,
-                            enlargeCenterPage: true,
-                            itemCount: secondSlider.length,
-                            itemBuilder: (context, index) => Image.asset(
-                                  fit: BoxFit.fill,
-                                  secondSlider[index],
-                                )
-                                    .box
-                                    .rounded
-                                    .clip(Clip.antiAlias)
-                                    .margin(const EdgeInsets.symmetric(
-                                        horizontal: 8))
-                                    .make()),
-                        15.heightBox,
-                        Align(
-                            alignment: Alignment.topLeft,
-                            child: makeText(
-                                text: featureCategories,
-                                size: 18.0,
-                                fontweight: FontWeight.bold,
-                                color: Colors.grey)),
-                        10.heightBox,
-                        SingleChildScrollView(
-                          physics: const BouncingScrollPhysics(),
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: List.generate(
-                              3,
-                              (index) => Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        border: Border.all(color: Colors.white),
-                                        borderRadius: const BorderRadius.all(
-                                            Radius.circular(10))),
-                                    padding: const EdgeInsets.all(8),
-                                    child: Row(
-                                      // mainAxisAlignment:
-                                      //     MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Image.asset(
-                                          width: 25,
-                                          featureProductsIcons1[index],
-                                        ),
-                                        3.widthBox,
-                                        makeText(
-                                          text: featureProducts1[index],
-                                          fontFamily: mainFont,
-                                        )
-                                      ],
-                                    )
-                                        .box
-                                        .padding(const EdgeInsets.all(8))
-                                        .make(),
-                                  ),
-                                  8.heightBox,
-                                  Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        border: Border.all(color: Colors.white),
-                                        borderRadius: const BorderRadius.all(
-                                            Radius.circular(10))),
-                                    padding: const EdgeInsets.all(8),
-                                    child: Row(
-                                      children: [
-                                        Image.asset(
-                                          width: 25,
-                                          featureProductsIcons2[index],
-                                        ),
-                                        3.widthBox,
-                                        makeText(
-                                          text: featureProducts2[index],
-                                          fontFamily: mainFont,
-                                        )
-                                      ],
-                                    )
-                                        .box
-                                        .padding(const EdgeInsets.all(8))
-                                        .make(),
-                                  ).box.make(),
-                                ],
-                              )
-                                  .box
-                                  .margin(const EdgeInsets.only(right: 5))
-                                  .make(),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+                );
+              }),
             ),
-          ),
-        ));
+            // Other widgets here
+          ],
+        ),
+      ),
+    );
+
+    //   SafeArea(
+    //     child: Padding(
+    //       padding: const EdgeInsets.all(12),
+    //       child: Column(
+    //         children: [
+    //           Container(
+    //             padding: const EdgeInsets.all(8),
+    //             width: double.infinity,
+    //             decoration: BoxDecoration(
+    //                 color: Colors.white,
+    //                 borderRadius: BorderRadius.circular(20)),
+    //             child: makeText(
+    //                 text: 'Search anything', size: 14.0, fontFamily: mainFont),
+    //           ),
+    //           10.heightBox,
+    //           VxSwiper.builder(
+    //             height: 150,
+    //             autoPlay: true,
+    //             aspectRatio: 16 / 9,
+    //             itemCount: secondSlider.length,
+    //             enlargeCenterPage: true,
+    //             itemBuilder: (context, index) => Image.asset(
+    //               secondSlider[index],
+    //               fit: BoxFit.fill,
+    //             )
+    //                 .box
+    //                 .rounded
+    //                 .clip(Clip.antiAlias)
+    //                 .margin(const EdgeInsets.symmetric(horizontal: 8))
+    //                 .make(),
+    //           ),
+    //           15.heightBox,
+    //           Expanded(
+    //             child: GridView.builder(
+    //                 shrinkWrap: true,
+    //                 physics: const NeverScrollableScrollPhysics(),
+    //                 gridDelegate:
+    //                     const SliverGridDelegateWithFixedCrossAxisCount(
+    //                   crossAxisCount: 3,
+    //                 ),
+    //                 itemCount: 300,
+    //                 itemBuilder: (BuildContext context, int index) {
+    //                   return Card(
+    //                     color: Colors.amber,
+    //                     child: Center(child: Text('$index')),
+    //                   );
+    //                 }),
+    //           )
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
